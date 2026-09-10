1f721df7fa216007d04cea99a6e0eb1657a82548
CinaGanteng55 <rafidk1129@gmail.com>
2026-09-10 08:48:36 +0700

    Add files via upload
    ARSYA

---
+++ arrayass.php
@@ -0,0 +1,14 @@
+<?php
+
+$data_siswa=[
+    "dataSiswa1" => "Arsya  1234  16  gaming",
+    "dataSiswa2" => "Rapa   1111  16  tidur",
+    "dataSiswa3" => "Maldini  2222  16  mancing",
+];
+echo "Data siswa 1 " , $data_siswa["dataSiswa1"];
+echo "<br/>";
+echo "Data siswa 2 " , $data_siswa["dataSiswa2"];
+echo "<br/>";
+echo "Data siswa 3 " , $data_siswa["dataSiswa3"];
+echo "<br/>";
+
